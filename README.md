e9fd81c4fa35ac527dbe7d5b116244118a350045
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-02-14 12:52:26 +0100

    README

---
+++ README.md
@@ -0,0 +1,12 @@
+This repo is a demo implementation of few akka patterns I stumbled upon.
+
+More information about each pattern is at the top of it's scala source file. To gain most from this repo, you should study the implementation closely.
+
+* Extra
+
+This pattern can be used to help with aggregating responses from downstream actors in a non blocking fashion (without using ask pattern). More information about it can be found in [Effective Akka](http://shop.oreilly.com/product/0636920028789.do) book by Jamie Allen
+
+
+### Contributing
+
+This is by no means a complete directory of akka patterns, so contributions are always welcome.
--- src/main/scala/Extra.scala
+++ src/main/scala/Extra.scala
@@ -1,44 +1,65 @@
 import akka.actor._
 
-object Extra {
+case class GetExchangeRates(currencyPair: (String, String))
 
-  case class GetExchangeRates(currencyPair: (String, String))
+case class Exchange(exchangeName: String, exchangeActor: ActorRef)
+case class ExchangeRatePair(exchangeName: String, currencyPair: (String, String), rate: BigDecimal)
+case class ExchangeRateResults(currencyPair: (String, String), rates: Map[String, BigDecimal])
+case class ExchangeRateRequest(currencyPair: (String, String))
 
-  case class Exchange(exchangeName: String, exchangeActor: ActorRef)
-  case class ExchangeRatePair(exchangeName: String, currencyPair: (String, String), rate: BigDecimal)
-  case class ExchangeRateResults(currencyPair: (String, String), rates: Map[String, BigDecimal])
-  case class ExchangeRateRequest(currencyPair: (String, String))
+class ExchangeRatesActor(exchanges: List[Exchange]) extends Actor {
 
-  class ExchangeRatesActor(exchanges: List[Exchange]) extends Actor {
+  override def receive = {
+    case GetExchangeRates(currencyPair) =>
+      context.actorOf(Props(new Actor() {
+        val results: scala.collection.mutable.Map[String, Option[BigDecimal]] = scala.collection.mutable.Map() ++ exchanges.map(e => (e.exchangeName, None)).toMap
+        val originalSender = sender
 
-    override def receive = {
-      case GetExchangeRates(currencyPair) =>
-        context.actorOf(Props(new Actor() {
-          val results: scala.collection.mutable.Map[String, Option[BigDecimal]] = scala.collection.mutable.Map() ++ exchanges.map(e => (e.exchangeName, None)).toMap
-          val originalSender = sender
+        override def receive = {
+          case ExchangeRatePair(exchangeName, _, rate) =>
+            results(exchangeName) = Some(rate)
+            respondIfDone()
 
-          override def receive = {
-            case ExchangeRatePair(exchangeName, _, rate) =>
-              results(exchangeName) = Some(rate)
-              respondIfDone()
+        }
 
+        def respondIfDone() = {
+          if (!results.values.exists(_.isEmpty)) {
+            originalSender ! ExchangeRateResults(currencyPair, results.map(result => (result._1, result._2.get)).toMap)
           }
-
-          def respondIfDone() = {
-            if (!results.values.exists(_.isEmpty)) {
-              originalSender ! ExchangeRateResults(currencyPair, results.map(result => (result._1, result._2.get)).toMap)
-            }
-          }
-        }))
-    }
+        }
+      }))
   }
-
-  abstract class ExchangeActor(exchangeName: String) extends Actor
-
-  class StableExchangeActor(exchangeName: String) extends ExchangeActor(exchangeName) {
-    override def receive = {
-      case ExchangeRateRequest(currencyPair) => sender ! ExchangeRatePair(exchangeName, currencyPair, BigDecimal("4.22"))
-    }
-  }
-
 }
+
+abstract class ExchangeActor(exchangeName: String) extends Actor
+
+class StableExchangeActor(exchangeName: String) extends ExchangeActor(exchangeName) {
+  override def receive = {
+    case ExchangeRateRequest(currencyPair) => sender ! ExchangeRatePair(exchangeName, currencyPair, BigDecimal("4.22"))
+  }
+}
+
+class RandomExchangeActor(exchangeName: String) extends ExchangeActor(exchangeName) {
+  override def receive = {
+    case ExchangeRateRequest(currencyPair) =>
+      val r = scala.util.Random
+      val variablePart = r.nextDouble / 2.0
+      val rate = 4 + variablePart
+      sender ! ExchangeRatePair(exchangeName, currencyPair, BigDecimal(rate))
+  }
+}
+
+class DelayingExchangeActor(exchangeName: String) extends ExchangeActor(exchangeName) {
+  override def receive = {
+    case ExchangeRateRequest(currencyPair) =>
+      Thread.sleep(100)
+      sender ! ExchangeRatePair(exchangeName, currencyPair, BigDecimal("4.22"))
+  }
+}
+
+class TimingOutExchangeActor(exchangeName: String) extends ExchangeActor(exchangeName) {
+  override def receive = {
+    case ExchangeRateRequest(currencyPair) => // it just never responds
+  }
+}
+
